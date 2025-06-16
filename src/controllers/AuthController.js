be629b29cc484c5a6deeb0b054475f2ce0a81ca3
cassio <cassio8976@gmail.com>
2025-06-16 00:24:23 -0300

    modified:   src/app.js 	modified:   src/controllers/AuthController.js

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -8,7 +8,7 @@ class AuthController {
       res.cookie("auth-token", result.token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        sameSite: "lax",
         maxAge: 24 * 60 * 60 * 1000,
         path: "/",
       });
